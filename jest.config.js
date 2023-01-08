695eab4b7f61b3e9607d42212d33c3c09e7adf2a
Dimas Paiva <Dimas.Paiva@ctw.bmwgroup.com>
2023-01-08 16:27:22 +0000

    chore: improve jest configs and scripts

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverage: true,
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/protocols/**/*.ts',
+    '!<rootDir>/src/**/*protocols.ts',
+    '!<rootDir>/src/**/domain/**/*.ts'
+  ],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   testEnvironment: 'node',
