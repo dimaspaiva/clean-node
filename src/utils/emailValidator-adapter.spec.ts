694d33614b8a96f7a0f28015d03cb8915f594e4c
Dimas Paiva <Dimas.Paiva@ctw.bmwgroup.com>
2023-01-08 17:20:15 +0000

    chore: move emailValidator to a facory function

--- src/utils/emailValidator-adapter.spec.ts
+++ src/utils/emailValidator-adapter.spec.ts
@@ -1,23 +1,27 @@
 import validator from 'validator'
 import { EmailValidatorAdapter } from './emailValidator-adapter'
 
+const makeSut = (): EmailValidatorAdapter => {
+  return new EmailValidatorAdapter()
+}
+
 describe('Email validator adapter', () => {
   test('should return false if validator returns false', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const isValid = sut.isValid('invalid@mail.com')
     expect(isValid).toBeFalsy()
   })
 
   test('should return true if validator returns true', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(true)
     const isValid = sut.isValid('valid@mail.com')
     expect(isValid).toBeTruthy()
   })
 
   test('should return true if validator returns true', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
     const emailValidatorSpy = jest.spyOn(validator, 'isEmail').mockReturnValueOnce(true)
     const isValid = sut.isValid('valid@mail.com')
     expect(isValid).toBeTruthy()
