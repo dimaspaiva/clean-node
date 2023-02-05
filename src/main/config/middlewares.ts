46bcb3b624e7bea08c427c4f1d0575ab4ebd1285
Dimas Paiva <Dimas.Paiva@ctw.bmwgroup.com>
2023-02-05 23:26:36 +0000

    feat: ensure cors is enabled

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,9 @@
 import { Express } from 'express'
+
 import { bodyParse } from '../middlewares/bodyParse'
+import { cors } from '../middlewares/cors'
 
 export default (app: Express): void => {
   app.use(bodyParse)
+  app.use(cors)
 }
